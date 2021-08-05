7994c5432e533ab18ad723875b6e15335d26f059
yuanfeng <1600120583@qq.com>
2021-08-05 23:46:43 +0800

    new出5个敌人坦克

--- tank/src/com/mashibing/TankFrame.java
+++ tank/src/com/mashibing/TankFrame.java
@@ -11,8 +11,10 @@ import java.util.List;
 
 public class TankFrame extends Frame {
 
-    Tank myTank=new Tank(200,200,Dir.DOWN,this);
+    Tank myTank=new Tank(200,400,Dir.DOWN,this);
     List<Bullet> bullets=new ArrayList<>();
+    List<Tank> tanks=new ArrayList<>();
+
     Bullet b = new Bullet(200,200,Dir.DOWN,this);
     static final int GAME_WIDTH = 800,GAME_HEIGHT=600;
 
@@ -57,6 +59,9 @@ public class TankFrame extends Frame {
         g.drawString("子弹的数量：" + bullets.size(),10,60);
         g.setColor(c);
         myTank.paint(g);
+        for (int i = 0; i < tanks.size(); i++) {
+            tanks.get(i).paint(g);
+        }
         for (int i = 0; i < bullets.size(); i++) {
             bullets.get(i).paint(g);
         }
