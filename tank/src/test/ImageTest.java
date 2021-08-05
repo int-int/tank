7575a02da553f70b44d7ea572bfc55c5c37e7ca6
yuanfeng <1600120583@qq.com>
2021-08-05 23:14:25 +0800

    添加ResourceMgr类，管理资源，把图片load到内存，坦克换成图片

--- tank/src/test/ImageTest.java
+++ tank/src/test/ImageTest.java
@@ -14,8 +14,11 @@ public class ImageTest {
     public void test(){
 
         try {
-            BufferedImage read = ImageIO.read(new File("C:/Users/DELL/Desktop/ps/沙漠.png"));
-            Assert.assertNotNull(read);
+            BufferedImage image = ImageIO.read(new File("C:/Users/DELL/Desktop/ps/沙漠.png"));
+            Assert.assertNotNull(image);
+
+            BufferedImage image1 = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            Assert.assertNotNull(image1);
         } catch (IOException e) {
             e.printStackTrace();
         }
