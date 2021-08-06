ccf319a506569482d35dafd6cc52642fab16281f
yuanfeng <1600120583@qq.com>
2021-08-06 23:43:58 +0800

    加入配置文件，让项目更灵活

--- tank/src/com/mashibing/tank/Main.java
+++ tank/src/com/mashibing/tank/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf=new TankFrame();
 
+        int initTankCount = Integer.parseInt(PropertyMgr.get("initTankCount"));
+
         //初始化地方坦克
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < initTankCount; i++) {
             tf.tanks.add(new Tank(50 + i*80,200,Dir.DOWN,Group.BAD,tf));
         }
         //new Thread(()-> new Audio("audio/war1.wav").play()).start();
